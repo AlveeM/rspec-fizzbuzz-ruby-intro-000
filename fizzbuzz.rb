fe650eef5e61dbbd417283bbcdcca65752a1bcf7
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-08 17:11:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
     return "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
   else
